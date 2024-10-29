a70f2e5d39aa39886a12657818e56885debfede7
Dominic Madeira <dominic.madeira@mths.ca>
2024-10-29 14:06:18 +0000

    pushed changes

---
+++ 5/ts/Vehicle.ts
@@ -0,0 +1,35 @@
+/**
+ * The Vehicle Class
+ *
+ * author Dominic M.
+ * version 1.0
+ * since 2024-10-28
+ */
+
+export class Vehicle {
+  /**
+   * The license plate
+   */
+  private licensePlate: number
+  /**
+   * The color of car
+   */
+  private color: string
+  /**
+   * The amount of doors
+   */
+  private readonly doorsNumber: number
+  /**
+   * The speed of car
+   */
+  private readonly speed: number
+  /**
+   * The max speed
+   */
+  private readonly maxSpeed: number
+
+  constructor(licensePlate: number, color: string, doorsNumber: number, speed: number, maxSpeed: number){
+
+  }
+  // getters
+}
