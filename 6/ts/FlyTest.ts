8642224da33ada9cb49482aff46d2b0b2555d131
Dominic Madeira <dominic.madeira@mths.ca>
2024-11-06 02:41:02 +0000

    pushed changes

---
+++ 6/ts/FlyTest.ts
@@ -0,0 +1,29 @@
+/**
+ * The FlyTest.ts starting point.
+ *
+ * author Dominic M.
+ * since 2024-9-22
+ * version 1.0
+ */
+
+import { Airplane } from './Airplane.ts'
+import { Jet } from './Jet.ts'
+
+const biplane = new Airplane()
+biplane.theSpeed = 212
+console.log(biplane.theSpeed)
+const boeing = new Jet()
+boeing.theSpeed = 422
+console.log(boeing.theSpeed)
+let x = 0
+while (x<4) {
+  boeing.accelerate()
+  console.log(boeing.theSpeed)
+  if (boeing.theSpeed < 5000) {
+    biplane.theSpeed = biplane.theSpeed * 2
+  } else {
+    boeing.accelerate()
+  }
+  x++
+}
+console.log(biplane.theSpeed)
