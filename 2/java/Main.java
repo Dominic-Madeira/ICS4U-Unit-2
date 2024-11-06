c033c23353adba0f5df060230a067632a46a11e8
Dominic Madeira <dominic.madeira@mths.ca>
2024-11-06 07:29:36 +0000

    pushed changes

---
+++ 2/java/Main.java
@@ -0,0 +1,63 @@
+/*
+ * The Starting point for the script.
+ *
+ * @author Dominic M.
+ * @since 2024-10-22
+ * @version 1.0
+ */
+
+/**
+ * This is the Main class.
+ */
+final class Main {
+
+    /**
+     * Prevent instantiation.
+     *
+     * @throws IllegalStateException if this is ever called
+     *
+     */
+    private Main() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+     * This is the main function.
+     *
+     * @param args no args used
+     */
+    public static void main(final String[] args) {
+        // Use MrCoxallStack
+        final MrCoxallStack fruitStack = new MrCoxallStack();
+        fruitStack.pushString("apple");
+        fruitStack.pushString("banana");
+        fruitStack.pushString("cherry");
+        System.out.println("Fruit items: " + fruitStack.getStack());
+        final String topFruit = fruitStack.popItem();
+        System.out.println("Popped fruit item: " + topFruit);
+        System.out.println("Fruit items: " + fruitStack.getStack());
+        System.out.println();
+
+        final MrCoxallStack colorStack = new MrCoxallStack();
+        colorStack.pushString("red");
+        colorStack.pushString("green");
+        colorStack.pushString("blue");
+        System.out.println("Color items: " + colorStack.getStack());
+        final String topColor = colorStack.popItem();
+        System.out.println("Popped color item: " + topColor);
+        System.out.println("Color items: " + colorStack.getStack());
+        System.out.println();
+
+        final MrCoxallStack doggyPile = new MrCoxallStack();
+        doggyPile.pushString("poodle");
+        doggyPile.pushString("retriever");
+        doggyPile.pushString("beagle");
+        System.out.println("Dog items: " + doggyPile.getStack());
+        final String topDog = doggyPile.popItem();
+        System.out.println("Popped dog item: " + topDog);
+        System.out.println("Dog items: " + doggyPile.getStack());
+        System.out.println();
+
+        System.out.println("\nDone.");
+    }
+}
---
+++ 2/java/MrCoxallStack.java
@@ -0,0 +1,69 @@
+/*
+ * The MrCoxallStack Class.
+ *
+ * @author Dominic M.
+ * @since 2024-10-22
+ * @version 1.0
+ */
+
+import java.util.ArrayList;
+
+/**
+ * This is the MrCoxallStack class.
+ */
+public class MrCoxallStack {
+
+    /**
+     * The MrCoxallStack list.
+     */
+    private ArrayList<String> stackList;
+
+    /**
+     * The constructor for the stack.
+     */
+    public MrCoxallStack() {
+        this.stackList = new ArrayList<String>();
+    }
+
+     /**
+     * This method returns the stack as a string.
+     *
+     * @return the stack as a string
+     */
+    public String getStack() {
+        String stackValues = "";
+        for (int counter = 0; counter < this.stackList.size(); counter++) {
+            stackValues += this.stackList.get(counter) + ", ";
+        }
+        stackValues = stackValues.substring(0, stackValues.length() - 2);
+        return stackValues;
+    }
+
+    /**
+     * This method pushes a string into the stack.
+     *
+     * @param pushedString - The string to push into the stack
+     */
+    public void pushString(final String pushedString) {
+        this.stackList.add(pushedString);
+    }
+
+    /**
+     * This method pops a string from the stack.
+     *
+     * @return string popped from stack
+     */
+    public String popItem() {
+        String returnString = "";
+        if (this.stackList.size() > 0) {
+            final String topItem = this.stackList.get(
+                this.stackList.size() - 1
+            );
+            this.stackList.remove(this.stackList.size() - 1);
+            returnString = topItem;
+        } else {
+            returnString = "Invalid Input!";
+        }
+        return returnString;
+    }
+}
