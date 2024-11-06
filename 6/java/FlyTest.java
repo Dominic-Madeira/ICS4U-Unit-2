8795f457f32485782a76996d2f7220d47fda150d
Dominic Madeira <dominic.madeira@mths.ca>
2024-11-06 01:01:09 +0000

    pushed changes

---
+++ 6/java/FlyTest.java
@@ -0,0 +1,48 @@
+/**
+ * The FlyTest Class.
+ *
+ * @author Dominic M.
+ * @since 2024-9-22
+ * @version 1.0
+ */
+
+/**
+ * This is the FlyTest Class
+ */
+public class FlyTest {
+
+    /**
+     * Prevent instantiation.
+     *
+     * @throws IllegalStateException if this is ever called
+     *
+     */
+    private FlyTest() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+     * This is the main function.
+     * @param args no args used
+     */
+    public static void main(String[] args) {
+        Airplane biplane = new Airplane();
+        biplane.setSpeed(212);
+        System.out.println(biplane.getSpeed());
+        Jet boeing = new Jet();
+        boeing.setSpeed(422);
+        System.out.println(boeing.getSpeed());
+        int x = 0;
+        while (x<4) {
+            boeing.accelerate();
+            System.out.println(boeing.getSpeed());
+                if (boeing.getSpeed() > 5000) {
+                    biplane.setSpeed(biplane.getSpeed() * 2);
+                } else {
+                    boeing.accelerate();
+                }
+            x++;
+        }
+        System.out.println(biplane.getSpeed());
+    }
+}
